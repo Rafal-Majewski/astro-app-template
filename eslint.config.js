b5f4ab69c51824e1db2f58fcaf77f9abd0cae64a
Rafał Majewski <goodheropl@gmail.com>
2023-04-24 17:05:21 +0200

    Add ESLint astro plugin

--- eslint.config.js
+++ eslint.config.js
@@ -5,11 +5,13 @@
 import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
 import * as typescriptEslintParser from "@typescript-eslint/parser";
 import * as espreeParser from "espree";
+import * as AstroEslintParser from "astro-eslint-parser";
+import astroEslintPlugin from "eslint-plugin-astro";
 import globals from "globals";
 
 const eslintConfig = [
 	{
-		files: ["**/*.mjs", "**/*.js", "**/*.ts"],
+		files: ["**/*.mjs", "**/*.js", "**/*.ts", "**/*.astro"],
 		languageOptions: {
 			sourceType: "module",
 		},
@@ -21,11 +23,18 @@ const eslintConfig = [
 		},
 	},
 	{
+		files: ["**/*.mjs", "**/*.js", "**/*.ts", "**/*.cjs", "**/*.astro"],
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
+	},
+	{
 		languageOptions: {
 			ecmaVersion: 13,
 			globals: {
 				...globals.es2021,
-				...globals.node,
 			},
 		},
 		rules: {
@@ -90,7 +99,7 @@ const eslintConfig = [
 		},
 	},
 	{
-		files: ["**/*.ts"],
+		files: ["**/*.ts", "**/*.astro"],
 		rules: {
 			"@typescript-eslint/adjacent-overload-signatures": "error",
 			"@typescript-eslint/ban-ts-comment": "error",
@@ -118,12 +127,15 @@ const eslintConfig = [
 		plugins: {
 			"@typescript-eslint": typescriptEslintPlugin,
 		},
+	},
+	{
+		files: ["**/*.ts"],
 		languageOptions: {
 			parser: typescriptEslintParser,
 			parserOptions: {
 				project: "./tsconfig.json",
 				tsconfigRootDir: ".",
-				extraFileExtensions: [],
+				extraFileExtensions: [".astro"],
 			},
 		},
 	},
@@ -131,6 +143,69 @@ const eslintConfig = [
 		files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
 		languageOptions: {
 			parser: espreeParser,
+		},
+	},
+	{
+		files: ["**/*.astro"],
+		languageOptions: {
+			parser: AstroEslintParser,
+			parserOptions: {
+				parser: typescriptEslintParser,
+				extraFileExtensions: [".astro"],
+			},
+		},
+		plugins: {
+			"astro": astroEslintPlugin,
+		},
+		rules: {
+			"astro/no-conflict-set-directives": "error",
+			"astro/no-deprecated-astro-canonicalurl": "error",
+			"astro/no-deprecated-astro-fetchcontent": "error",
+			"astro/no-deprecated-astro-resolve": "error",
+			"astro/no-unused-define-vars-in-style": "error",
+			"astro/valid-compile": "error",
+			"astro/no-set-html-directive": "error",
+			"astro/no-set-text-directive": "error",
+			"astro/no-unused-css-selector": "error",
+			"astro/prefer-class-list-directive": "error",
+			"astro/prefer-object-class-list": "error",
+			"astro/prefer-split-class-list": "error",
+			"astro/jsx-a11y/alt-text": "error",
+			"astro/jsx-a11y/anchor-ambiguous-text": "error",
+			"astro/jsx-a11y/anchor-has-content": "error",
+			"astro/jsx-a11y/anchor-is-valid": "error",
+			"astro/jsx-a11y/aria-activedescendant-has-tabindex": "error",
+			"astro/jsx-a11y/aria-props": "error",
+			"astro/jsx-a11y/aria-proptypes": "error",
+			"astro/jsx-a11y/aria-role": "error",
+			"astro/jsx-a11y/aria-unsupported-elements": "error",
+			"astro/jsx-a11y/autocomplete-valid": "error",
+			"astro/jsx-a11y/click-events-have-key-events": "error",
+			"astro/jsx-a11y/control-has-associated-label": "error",
+			"astro/jsx-a11y/heading-has-content": "error",
+			"astro/jsx-a11y/html-has-lang": "error",
+			"astro/jsx-a11y/iframe-has-title": "error",
+			"astro/jsx-a11y/img-redundant-alt": "error",
+			"astro/jsx-a11y/interactive-supports-focus": "error",
+			"astro/jsx-a11y/label-has-associated-control": "error",
+			"astro/jsx-a11y/lang": "error",
+			"astro/jsx-a11y/media-has-caption": "error",
+			"astro/jsx-a11y/mouse-events-have-key-events": "error",
+			"astro/jsx-a11y/no-access-key": "error",
+			"astro/jsx-a11y/no-aria-hidden-on-focusable": "error",
+			"astro/jsx-a11y/no-autofocus": "error",
+			"astro/jsx-a11y/no-distracting-elements": "error",
+			"astro/jsx-a11y/no-interactive-element-to-noninteractive-role": "error",
+			"astro/jsx-a11y/no-noninteractive-element-interactions": "error",
+			"astro/jsx-a11y/no-noninteractive-element-to-interactive-role": "error",
+			"astro/jsx-a11y/no-noninteractive-tabindex": "error",
+			"astro/jsx-a11y/no-redundant-roles": "error",
+			"astro/jsx-a11y/no-static-element-interactions": "error",
+			"astro/jsx-a11y/prefer-tag-over-role": "error",
+			"astro/jsx-a11y/role-has-required-aria-props": "error",
+			"astro/jsx-a11y/role-supports-aria-props": "error",
+			"astro/jsx-a11y/scope": "error",
+			"astro/jsx-a11y/tabindex-no-positive": "error",
 		},
 	},
 	{
